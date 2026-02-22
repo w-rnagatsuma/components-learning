20bf012b4355f87c88943a6eed5503968577d5cf
r.nagatsuma <w.ryuuya.nagatsuma@outlook.jp>
2026-02-22 20:19:53 +0900

    nuxt3 初期設定完了

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15', // 互換性のための基準日
+  devtools: { enabled: true }, // Nuxt DevTools 有効
+  modules: ['@nuxt/eslint'],
+});
