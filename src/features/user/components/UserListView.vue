a8c0790e93d1357e85f073b053a7de9174bded00
r.nagatsuma <w.ryuuya.nagatsuma@outlook.jp>
2026-02-28 23:20:29 +0900

    feat: UserListView.vue作成中

---
+++ src/features/user/components/UserListView.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <header>
+      <h2>ユーザ一覧</h2>
+      <span>loading...</span>
+    </header>
+
+    <BaseInput v-model="qProxy" label="検索" placeholder="検索ワードを入力してください" />
+
+    <p>{{ error }}</p>
+
+    <ul>
+      <li>
+        <a>dummy</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import BaseInput from '~/src/shared/components/BaseInput.vue';
+
+const qProxy = ref('');
+const error = ref('');
+</script>
+
+<style scoped></style>
