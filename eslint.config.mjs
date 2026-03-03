db272848ca610fd6ab23c6671aac6b26a2a44e12
r.nagatsuma <w.ryuuya.nagatsuma@outlook.jp>
2026-03-03 12:07:09 +0900

    feat: ページ設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default withNuxt(
-  eslintConfigPrettier
-)
+export default withNuxt(eslintConfigPrettier, {
+  // Nuxtのページコンポーネントは単語数が1つになることが多いので、ルールをオフにする
+  files: ['pages/**/*.vue', 'src/pages/**/*.vue'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15', // 互換性のための基準日
   devtools: { enabled: true }, // Nuxt DevTools 有効
   modules: ['@nuxt/eslint'],
+  dir: {
+    pages: 'src/pages',
+  },
 });
