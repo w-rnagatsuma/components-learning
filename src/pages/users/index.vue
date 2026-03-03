466f00f2dc9c5100451e9076463b45f66ca9caf8
r.nagatsuma <w.ryuuya.nagatsuma@outlook.jp>
2026-03-03 10:58:20 +0900

    feat: users/index.vueを作成

---
+++ src/pages/users/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>ユーザー一覧</h1>
+    <UserListContainer />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import UserListContainer from '~/src/features/user/components/UserListContainer.vue';
+
+useSeoMeta({
+  title: 'Users',
+  description: 'User list (demo)',
+});
+</script>
+
+<style scoped></style>
