c7b1f5ccb49b2b0afb4271ca1b949f4db6c9da8d
r.nagatsuma <w.ryuuya.nagatsuma@outlook.jp>
2026-03-03 12:05:51 +0900

    feat: users/[id].vueを作成

---
+++ src/pages/users/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="page">
+    <UserDetailContainer :id="id" />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import UserDetailContainer from '~/src/features/user/components/UserDetailContainer.vue';
+
+const route = useRoute();
+const id = computed(() => String(route.params.id));
+
+console.log(route.params);
+</script>
+
+<style scoped>
+.page {
+  padding: 18px;
+}
+</style>
